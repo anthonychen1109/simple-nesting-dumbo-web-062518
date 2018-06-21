b486dc65ba71918f087931e349fa3f77ab4eddeb
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-21 02:05:34 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,6 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:alan_kay][:known_for]
 	
 	programmer_hash = 
  		{
@@ -67,6 +66,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		   	:yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -102,6 +105,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
      
 end
