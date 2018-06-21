967ab2f8d72f1081cfcaa1108800e6edace10b59
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-21 02:14:02 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -57,6 +57,7 @@ def dennis_ritchies_language
         }
      }
      programmer_hash[:dennis_ritchie][:languages].to_s
+     programmer_hash
 end
 
 def adding_matz
@@ -130,5 +131,6 @@ def adding_to_dennis
         }
      }
      programmer_hash[:dennis_ritchie][:languages].push('Assembly')
+     programmer_hash
 
 end
